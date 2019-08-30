3fb984deed7680af0aa0f697026d71d12de89d5d
Daniel Park <parkdk96@gmail.com>
2019-08-30 05:13:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,8 +51,8 @@ end
 
 def checkout(cart, coupons)
   cart = consolidate_cart(cart)
-  cart = apply_coupons(cart)
-  cart = (apply_clearance)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
   
   total = 0 
   cart.each do |item, specs|
